7a35b618389d86e8168e030c9ec8940a0d31e3b4
khaily <ly.k@husky.neu.edu>
2020-04-02 14:43:00 -0700

    added quizes controller

---
+++ controllers/quizzes.controller.server.js
@@ -0,0 +1,14 @@
+const quizzesService
+    = require('../services/quizzes.services.server')
+
+module.exports = function (app) {
+
+    app.get('/api/quizzes', (req, res) =>
+        res.json(quizzesService
+            .findAllQuizzes()))
+
+    app.get('/api/quizzes/:qzid', (req, res) =>
+        res.json(quizzesService
+            .findQuizById(req.params['qzid']))
+    )
+}
